0ffeee2f391132274c09d5c4efb63a0f38faf79a
spavansaireddy <122297514+spavansaireddy@users.noreply.github.com>
2023-02-11 14:07:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Data from './inputfield/frontend';
+
+function App() {
+  return (
+    <div className="App">
+      <Data />
+    </div>
+  );
+}
+
+export default App;
